d7e11b703b602356e016d9781347c31a795ee341
Dean Lofts <dean@deanlofts.xyz>
2025-02-22 20:10:14 +1100

    add pwa

---
+++ public/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'bogan-hustler-v1';
+const urlsToCache = [
+  '/bogan_hustler/',
+  '/bogan_hustler/index.html',
+  '/bogan_hustler/manifest.json',
+  '/bogan_hustler/favicon.ico',
+  '/bogan_hustler/icon-192.png',
+  '/bogan_hustler/icon-512.png',
+  '/bogan_hustler/themesong.mp3',
+  '/bogan_hustler/siren.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
